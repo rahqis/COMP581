8c7bdde5851cf5158472c5d03d041bada38bba0f
Kaylee Tackett <kayleet@live.unc.edu>
2022-02-22 21:50:55 -0500

    tweaked some distances

--- Lab_1/main.py
+++ Lab_1/main.py
@@ -12,36 +12,31 @@ import math
 
 
 # Create your objects here.
-ev3 = EV3Brick()                        # Init lego brick
-left_motor = Motor(Port.B)              # Left wheel
-right_motor = Motor(Port.D)             # Right wheel
-bumper = TouchSensor(Port.S1)           # Init touch sensor
-angry_eyes = UltrasonicSensor(Port.S4)  # Init ultrasonic sensor
+ev3 = EV3Brick()
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.D)
+bumper = TouchSensor(Port.S1)
+angry_eyes = UltrasonicSensor(Port.S4)
 
+init_distance = angry_eyes.distance()/1000
+curr_distance = init_distance
+diameter = .06  # Meters
+circum = diameter * math.pi
+phase_1_time = (1.2 / (485/360 * circum))
 
-diameter = .06                          # Diameter of wheels in Meters
-circum = diameter * math.pi             # Circumference of wheels in Meters
-
-# Time to calculate for robot to move 1.2 meters.
-# If you want to change it, 500 is degrees/sec that the wheel moves
-# so play around with that number to adjust time needed
-# General formula: distance needed to travel / (rotational speed in degrees/360 * circumference)
-phase_1_time = 1.2 / (500/360 * circum)
-
-# Same rule for phase_1, this time distance is half a meter
-phase_3_time = .5 / (500/360 * circum)
+phase_3_time = (.5 / (300/360 * circum))
+start_time = 0
 
 # Phase 1
 
 
 def first_press():
-    watch = StopWatch()                     # Starts time in ms. 1000 ms = 1 second
+    watch = StopWatch()
     while True:
-        left_motor.run(500)                 # Wheels move at 500 degrees/sec
-        right_motor.run(500)
+        left_motor.run(485)
+        right_motor.run(485)
 
-        if watch.time()/1000 > phase_1_time:    # Once phase_1 time has passed, stop
-            print(watch.time())
+        if watch.time()/1000 > phase_1_time+0.6:
             left_motor.hold()
             right_motor.hold()
             break
@@ -52,26 +47,26 @@ def first_press():
 
 def second_press():
     while True:
-        if angry_eyes.distance()/1000 < .50:        # if distance is less than half a meter, stop
+        if angry_eyes.distance()/1000 < .57:
             break
-        watch = StopWatch()                         # Start watch
+        watch = StopWatch()
         while True:
-            if angry_eyes.distance()/1000 < .60:    # if distance is less than .6 meters, moves at 60 deg/sec
+            if angry_eyes.distance()/1000 < .60:
                 left_motor.run(60)
                 right_motor.run(60)
-            else:                                   # else move has 120 degs/sec
+            else:
                 left_motor.run(120)
                 right_motor.run(120)
             if watch.time() > 1000:
                 print(watch.time())
                 break
 
-        while True:                                 # after every second, stop wheels
+        while True:
             left_motor.hold()
             right_motor.hold()
-            if angry_eyes.distance()/1000 < .50:    # if robot is less than .50 meters, break
+            if angry_eyes.distance()/1000 < .50:
                 break
-            if watch.time() > 2000:                 # holds for 1 second to calculate, then breaks
+            if watch.time() > 2000:
                 print(watch.time())
                 break
         print(angry_eyes.distance()/1000)
@@ -81,42 +76,41 @@ def second_press():
 
 def third_press():
     while True:
-        left_motor.run(500)                             # Moves at 500 deg/sec
+        left_motor.run(500)
         right_motor.run(500)
-        if bumper.pressed():                            # if touch sensor is touched
-            watch = StopWatch()                         # Start watch
+        if bumper.pressed():
+            watch = StopWatch()
             while True:
-                # Move 500 deg/sec counterclockwise
-                left_motor.run(-500)
-                right_motor.run(-500)
+                left_motor.run(-300)
+                right_motor.run(-300)
 
-                if watch.time()/1000 > phase_3_time:    # Once stop watch reached phase 3 time, stop
-                    print(watch.time())
+                if watch.time()/1000 > phase_3_time+(0.5):
+                    print("s3")
                     break
             break
 
 
 def main():
-    count = 0                                       # Init count of pressed button
+    count = 0
     while True:
-        if Button.CENTER in ev3.buttons.pressed():  # If center button is pressed
-            count += 1                              # Add count by 1
-            if count == 1:                          # If button is pressed the first time, execute phase 1
+        if Button.CENTER in ev3.buttons.pressed():
+            count += 1
+            if count == 1:
                 while True:
                     first_press()
-                    ev3.speaker.beep()              # Beep
+                    ev3.speaker.beep()
                     break
-            elif count == 2:                        # If button is pressed for second time, execute phase 2
+            elif count == 2:
                 second_press()
-                ev3.speaker.beep()                  # Beep
-            elif count == 3:                        # If button is pressed for third time, execute phase 3
+                ev3.speaker.beep()
+            elif count == 3:
                 third_press()
 
-        if count is 3:                              # Once count is 3, break loop
+        if count is 3:
             break
     return
 
 
-main()                      # Run main
+main()
 # Write your program here.
 # ev3.speaker.beep()
